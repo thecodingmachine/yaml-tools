b96df6c33424c743a7ead67f41326e5be4ec4202
Jindun SHAO <sjindun@gmail.com>
2018-05-15 10:55:02 +0200

    Add sys.stdout as default output, add path when raising error

--- src/yaml_tools.py
+++ src/yaml_tools.py
@@ -1,24 +1,25 @@
+import sys
 import argparse
 import ruamel.yaml
 
 
-def get_type_error(dest, src):
-    return TypeError("Error trying to merge a {0} in a {1}".format(type(src), type(dest)))
+def get_type_error(dest, src, path):
+    return TypeError("Error trying to merge a {0} in a {1} at {2}".format(type(src), type(dest), path))
 
 
-def update(dest, src):
+def update(dest, src, path=""):
     if isinstance(src, ruamel.yaml.comments.CommentedMap):
         if isinstance(dest, ruamel.yaml.comments.CommentedMap):
             for k in src:
-                dest[k] = update(dest[k], src[k]) if k in dest else src[k]
+                dest[k] = update(dest[k], src[k], path+"."+str(k)) if k in dest else src[k]
                 if k in src.ca._items and src.ca._items[k][2] and \
                         src.ca._items[k][2].value.strip():
                     dest.ca._items[k] = src.ca._items[k]  # copy non-empty comment
         else:
-            raise get_type_error(dest, src)
+            raise get_type_error(dest, src, path)
     elif isinstance(src, ruamel.yaml.comments.CommentedSeq):
         if isinstance(dest, ruamel.yaml.comments.CommentedMap):
-            raise get_type_error(dest, src)
+            raise get_type_error(dest, src, path)
         elif isinstance(dest, ruamel.yaml.comments.CommentedSeq):
             dest.extend(src)
         else:
@@ -26,7 +27,7 @@ def update(dest, src):
             dest = src
     else:
         if isinstance(dest, ruamel.yaml.comments.CommentedMap):
-            raise get_type_error(dest, src)
+            raise get_type_error(dest, src, path)
         elif isinstance(dest, ruamel.yaml.comments.CommentedSeq):
             dest.append(src)
         else:
@@ -58,16 +59,16 @@ def merge_yaml(contents):
     for i in contents:
         data.append(ruamel.yaml.round_trip_load(i))
     for i in range(-1, -len(contents), -1):
-        update(data[i - 1], move_comment(data[i]))
+        update(data[i - 1], move_comment(data[i]), "ROOT")
     return data[0]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--inputs", nargs='+', type=str, help="<Required> List of input yaml files",
+    parser.add_argument("-i", "--inputs", nargs='+', type=str, help="<Required> List of input yaml files.",
                         required=True)
-    parser.add_argument("-o", "--output", type=str, help="Path to the output file", default="./output.yaml")
-    parser.add_argument("--indent", type=int, help="Number of space(s) for each indent", default=2)
+    parser.add_argument("-o", "--output", type=str, help="Path to the output file, or stdout by default.")
+    parser.add_argument("--indent", type=int, help="Number of space(s) for each indent.", default=2)
 
     args = parser.parse_args()
 
@@ -75,7 +76,8 @@ if __name__ == '__main__':
     for f in args.inputs:
         file_content = open(f, "r").read()
         file_contents.append(file_content)
-    print()
+
     out_content = merge_yaml(file_contents)
-    output_file = open(args.output, "w")
-    ruamel.yaml.round_trip_dump(out_content, output_file, indent=args.indent)
+    output = open(args.output, "w") if args.output else sys.stdout
+
+    ruamel.yaml.round_trip_dump(out_content, output, indent=args.indent)
