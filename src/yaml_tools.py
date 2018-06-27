b7e1ea259da521f2200d2c5f72c756db78fad97b
Jindun SHAO <sjindun@gmail.com>
2018-06-27 10:44:23 +0200

    fixing merge and delete, implementing comment

--- src/yaml_tools.py
+++ src/yaml_tools.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import argparse
-import re
 import sys
 from copy import deepcopy
 
@@ -11,196 +10,263 @@ from ruamel.yaml.error import StreamMark
 from ruamel.yaml.tokens import CommentToken
 
 
+##
+# MERGE
+#
+
+
 def get_type_error(dest, src, current_path):
-    return TypeError('Error trying to merge a {0} in a {1} at {2}'.format(type(src), type(dest), current_path))
+    return TypeError('Error trying to merge a {0} in a {1} at ({2})'.format(type(src), type(dest), current_path))
 
 
-def _merge(dest, src, current_path=""):
+def copy_ca_comment_and_ca_end(dest, src):
+    # ruamel.yaml.Comment.ca contains 3 attributes : comment, items and end. We just copy comment and end here
+    if src.ca and dest.ca:
+        if src.ca.comment is not None:
+            if dest.ca.comment is None:
+                dest.ca.comment = [None, None]
+            if src.ca.comment[0] is not None:
+                dest.ca.comment[0] = src.ca.comment[0]
+            if src.ca.comment[1] is not None and len(src.ca.comment[1]) > 0:
+                dest.ca.comment[1] = src.ca.comment[1]
+        if len(src.ca.end) > 0:
+            dest.ca.end = src.ca.end
+
+
+def merge(dest, src, current_path=""):
     """
-    (Recursively) merge a source object to an dest object (CommentedMap, CommentedSeq or other object)
+    (Recursively) merge a source object to an dest object (CommentedMap, CommentedSeq, scalar or None)
     and append the current position to current_path
     :return: the merged object
     """
     if isinstance(src, CommentedMap):
         if isinstance(dest, CommentedMap):
             for k in src:
-                dest[k] = _merge(dest[k], src[k], current_path +
-                                 '.' + str(k)) if k in dest else src[k]
-                if k in src.ca._items and src.ca._items[k][2] and \
-                        src.ca._items[k][2].value.strip():
-                    # copy non-empty comment
-                    dest.ca._items[k] = src.ca._items[k]
+                dest[k] = merge(dest[k], src[k], current_path + '->' + str(k)) if k in dest else src[k]
+                if k in src.ca.items and src.ca.items[k][2] and src.ca.items[k][2].value.strip():
+                    # copy non empty 'items' comments
+                    dest.ca.items[k] = src.ca.items[k]
+            copy_ca_comment_and_ca_end(dest, src)
         elif dest is None:
             return src
         else:
             raise get_type_error(dest, src, current_path)
     elif isinstance(src, CommentedSeq):
-        if isinstance(dest, CommentedMap):
-            raise get_type_error(dest, src, current_path)
-        elif isinstance(dest, CommentedSeq):
-            dest.extend(src)
+        if isinstance(dest, CommentedSeq):
+            for i in src:
+                dest.append(i)
+            copy_ca_comment_and_ca_end(dest, src)
+        elif isinstance(dest, (str, int, float, bool)):
+            src.append(dest)
+            return src
         elif dest is None:
             return src
         else:
-            src.append(dest)
+            raise get_type_error(dest, src, current_path)
+    elif isinstance(src, (str, int, float, bool)):
+        if isinstance(dest, CommentedSeq):
+            dest.append(src)
+        elif isinstance(dest, (str, int, float, bool)):
             dest = src
+        else:
+            raise get_type_error(dest, src, current_path)
     elif src is None:
         return dest
     else:
-        if isinstance(dest, CommentedMap):
-            raise get_type_error(dest, src, current_path)
-        elif isinstance(dest, CommentedSeq):
-            dest.append(src)
-        else:
-            dest = src
+        raise get_type_error(dest, src, current_path)
     return dest
 
 
 def successive_merge(contents):
     """
-    Successively merge a list of yaml contents by calling _merge()
+    Successively merge a list of yaml contents by calling merge()
     :param contents: list of yaml contents in str format
     :return: merged yaml in str format
     """
     data = []
     for i in contents:
-        data.append(round_trip_load(i))
+        data.append(round_trip_load(i, preserve_quotes=True))
     for i in range(-1, -len(contents), -1):
-        final_data = _merge(data[i - 1], data[i], 'ROOT')
+        final_data = merge(data[i - 1], data[i], 'ROOT')
     return final_data
 
 
-def has_valid_brackets(s):
+##
+# DELETE and COMMENT
+##
+
+
+def str_or_int_map(s):
+    return int(s) if is_int(s) else s
+
+
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def delete_yaml_item(data, path_to_key, data_contains_list=True):
     """
-    Check if the string s is in format "key[index]"
-    :param s: e.g. my_list[0]
-    :return: (True, key, index) or (None, None, None)
+    Delete a yaml item given its path_to_key (e.g. [foo 0 bar]), and its direct previous comment(s)
     """
-    list_regex = re.compile(r"\A\w+\[{1}\d+\]{1}\Z")
-    if list_regex.match(s) is not None:
-        key = s[:int(s.find('['))]
-        index = int(s[s.find('[') + 1: len(s) - 1])
-        return True, key, index
-    return None, None, None
+    if data_contains_list:
+        path_to_key = list(map(str_or_int_map, path_to_key))
 
+    parent = data.mlget(path_to_key[:-1], list_ok=data_contains_list) if len(path_to_key) > 1 else data
+    item_key = path_to_key[-1]
 
-def get_dict_item(dic, item):
-    """
-    Get one specific item from a dict
-    :param dic: the dict
-    :param item: a key or key[index], in string format
-    :return: the wanted item if found, otherwise raises an error
-    """
-    is_array, key, index = has_valid_brackets(item)
-    if is_array:  # if we want to access an item from an array
-        try:
-            got_item = dic[key][index]
-        except IndexError:
-            raise IndexError('list index out of range at "{}"'.format(item))
-        except KeyError:
-            raise TypeError("'{}' is not a list".format(key))
-
-    else:  # simple dict get
-        got_item = dic[item]
-    return got_item
-
-
-def get_dict_item_from_path(dic, path):
-    """
-    Utility function to get one specific item from a dict given his "path" (in str format, e.g. "key1.list_key[0].key2")
-    :return: the item if found
-    """
-    if path == '':
-        return dic
-
-    path_to_item = path.split('.')
-
-    curr = dic
-    for p in path_to_item:
-        curr = get_dict_item(curr, p)
-    return curr
-
-
-def delete_with_comments_preservation(parent, comment_key, ):
-    found = False
-    # don't just pop the value for comment_key that way you lose comments
-    # in the original YAML, instead deepcopy and delete what is not needed
-    block_2b_commented = deepcopy(parent)
-    previous_key = None
-    keys = range(len(parent)) if isinstance(
-        parent, CommentedSeq) else parent.keys()
-    for key in keys:
-        if key == comment_key:
-            found = True
+    if isinstance(parent, CommentedMap):
+        if item_key not in parent:
+            raise KeyError("the key \'{}\' does not exist".format(item_key))
+        preceding_comments = parent.ca.items.get(item_key, [None, None, None, None])[1]
+        del parent[item_key]
+    elif isinstance(parent, CommentedSeq):
+        if not is_int(item_key) or item_key >= len(parent):
+            raise RuntimeError("the key \'{}\' is not an integer or exceeds its parent's length".format(item_key))
         else:
-            if not found:
-                previous_key = key
-            del block_2b_commented[key]
-
-    # now delete the key and its value, but preserve its preceding comments
-    preceding_comments = parent.ca.items.get(
-        comment_key, [None, None, None, None])[1]
-    del parent[comment_key]
-
-    if previous_key is None:
-        if parent.ca.comment is None:
-            parent.ca.comment = [None, []]
-        comment_list = parent.ca.comment[1]
+            preceding_comments = deepcopy(parent.ca.items.get(item_key, [None, None, None, None])[1])
+            parent.pop(item_key)  # CommentedSet.pop(idx) automatically shifts all ca.items' indexes !
     else:
-        comment_list = parent[previous_key].ca.end = []
-        parent[previous_key].ca.comment = [None, None]
+        raise RuntimeError("Couldn't reach the last item following the path_to_key " + str(path_to_key))
 
-    if preceding_comments is not None:
-        comment_list.extend(preceding_comments)
-    return block_2b_commented, comment_list
+    return data, preceding_comments
 
 
-# see: https://stackoverflow.com/a/43927974
-def comment_block(parent, comment_key, indent, seq_indent, dept):
-    block_2b_commented, comment_list = delete_with_comments_preservation(
-        parent, comment_key)
-    # startmark can be the same for all lines, only column attribute is used
-    start_mark = StreamMark(None, None, None, indent * dept)
+def comment_yaml_item(data, path_to_key, data_contains_list=True):
+    """
+    (EXPERIMENTAL) Comment a yaml item given its path_to_key (e.g. [foo 0 bar]), with comment preservation
+    Inspired from https://stackoverflow.com/a/43927974 @cherrot
+    """
+    if data_contains_list:
+        path_to_key = list(map(str_or_int_map, path_to_key))
+
+    parent = data.mlget(path_to_key[:-1], list_ok=data_contains_list) if len(path_to_key) > 1 else data
+    item_key = path_to_key[-1]
+    deleted_item = item_key
+
+    next_key = None
+
+    if isinstance(parent, CommentedMap):
+        if item_key not in parent:
+            raise KeyError("the key \'{}\' does not exist".format(item_key))
+        # don't just pop the value for item_key that way you lose comments
+        # in the original YAML, instead deepcopy and delete what is not needed
+        block_copy = deepcopy(parent)
+        found = False
+        keys = [k for k in parent.keys()]
+        for key in reversed(keys):
+            if key == item_key:
+                found = True
+            else:
+                if not found:
+                    next_key = key
+                del block_copy[key]
+
+        # now delete the key and its value, but preserve its preceding comments
+        preceding_comments = parent.ca.items.get(item_key, [None, None, None, None])[1]
+
+        if next_key is None:
+            if parent.ca.comment is None:
+                parent.ca.comment = [None, []]
+            if parent.ca.comment[1] is None:
+                parent.ca.comment[1] = []
+            comment_list = parent.ca.comment[1]
+        else:
+            comment_list = parent.ca.items.get(next_key, [None, None, None, None])[1]
+            if comment_list is None:
+                parent.ca.items[next_key] = [None, [], None, None]
+                comment_list = parent.ca.items.get(next_key)[1]
+        if preceding_comments is not None:
+            for c in reversed(preceding_comments):
+                comment_list.insert(0, c)
+        del parent[item_key]
+    elif isinstance(parent, CommentedSeq):
+        if not is_int(item_key) or item_key >= len(parent):
+            raise RuntimeError("the key \'{}\' is not an integer or exceeds its parent's length".format(item_key))
+        else:
+            block_copy = deepcopy(parent)
+            for i in reversed(range(len(parent))):
+                if i != item_key:
+                    del block_copy[i]
+
+            next_key = item_key
+            preceding_comments = deepcopy(parent.ca.items.get(item_key, [None, None, None, None])[1])
+            parent.pop(item_key)  # CommentedSet.pop(idx) automatically shifts all ca.items' indexes !
+
+            if len(parent) == 1 or next_key == len(parent):
+                comment_list = parent.ca.end # TODO: fix this, the appended comments don't show up in some case
+            else:
+                comment_list = parent.ca.items.get(next_key, [None, None, None, None])[1]
+                if comment_list is None:
+                    parent.ca.items[next_key] = [None, [], None, None]
+                    comment_list = parent.ca.items.get(next_key)[1]
+
+            if preceding_comments is not None:
+                for c in reversed(preceding_comments):
+                    comment_list.insert(0, c)
+    else:
+        raise RuntimeError("Couldn't reach the last item following the path_to_key " + str(path_to_key))
+
+    key_dept = len(path_to_key) - 1
+    if is_int(path_to_key[-1]) and key_dept > 0:
+        key_dept = key_dept - 1
+    comment_list_copy = deepcopy(comment_list)
+    del comment_list[:]
+
+    start_mark = StreamMark(None, None, None, 2 * key_dept)
     skip = True
-    for line in round_trip_dump(block_2b_commented, indent=indent, block_seq_indent=seq_indent).splitlines(True):
+    for line in round_trip_dump(block_copy).splitlines(True):
         if skip:
-            if not line.startswith(comment_key + ':'):
+            if line.strip(' ').startswith('#'):  # and deleted_item not in line:
                 continue
             skip = False
         comment_list.append(CommentToken('#' + line, start_mark, None))
+    comment_list.extend(comment_list_copy)
 
-    return False
+    return data
+
+
+###
+# main and commands
+###
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='A set of CLI tools to manipulate YAML files (merge, delete, etc...) with comment preservation',
+        description='A set of CLI tools to manipulate YAML files (merge, delete, comment, etc...) \
+         with comment preservation',
         usage='''yaml-tools <command> [<args>]
-At the moment there are only two commands available:
+At the moment there are three commands available:
    merge     Merge two or more yaml files and preserve the comments
-   delete    Delete an item (and all its childs items) given its path from the input yaml file
-   comment   Comment an item (and all its childs items) given its path from the input yaml file''')
+   delete    Delete an item (and all its child items) given its path from the input yaml file
+   comment   Comment an item (and all its child items) given its path from the input yaml file''')
     parser.add_argument('command', help='Sub-command to run')
     # parse_args defaults to [1:] for args, but you need to
     # exclude the rest of the args too, or validation will fail
     args = parser.parse_args(sys.argv[1:2])
     if args.command == 'merge':
-        merge()
+        merge_command()
     elif args.command == 'delete':
-        delete()
+        delete_command()
+    elif args.command == 'comment':
+        comment_command()
     else:
         print('Unrecognized command')
         parser.print_help()
         exit(1)
 
 
-def merge():
+def merge_command():
     """
     Sub-command, see main()
     """
     parser = argparse.ArgumentParser(
         description='Merge two or more yaml files and preserve the comments')
-    parser.add_argument('-i', '--inputs', nargs='+', type=str, help='<Required> List of input yaml files',
+    parser.add_argument('-i', '--inputs', nargs='+', type=str,
+                        help='<Required> List of input yaml files, merged from the last to the first',
                         required=True)
     parser.add_argument('-o', '--output', type=str,
                         help='Path to the output file, or stdout by default')
@@ -217,18 +283,18 @@ def merge():
 
     out_content = successive_merge(file_contents)
     output_file = open(args.output, 'w') if args.output else sys.stdout
-    round_trip_dump(out_content, output_file, indent=args.indent)
+    round_trip_dump(out_content, output_file)
     output_file.close()
 
 
-def delete():
+def delete_command():
     """
     Sub-command, see main()
     """
     parser = argparse.ArgumentParser(
         description='Delete one item from the input yaml file')
-    parser.add_argument('item_path', type=str,
-                        help='<Required> Yaml item to be deleted, e.g. "key1.list[0].key2"')
+    parser.add_argument('path_to_key', type=str, nargs='+',
+                        help='<Required> Yaml item to be deleted, e.g. "foo 0 bar"')
     parser.add_argument('-i', '--input', type=str,
                         help='<Required> Path to the input yaml files', required=True)
     parser.add_argument('-o', '--output', type=str,
@@ -238,27 +304,41 @@ def delete():
 
     args = parser.parse_args(sys.argv[2:])
     input_file = open(args.input, 'r')
-    data = round_trip_load(input_file.read())
+    data = round_trip_load(input_file.read(), preserve_quotes=True)
     input_file.close()
 
-    path_list = args.item_path.split('.')
-    item_parent = get_dict_item_from_path(data, '.'.join(path_list[:-1]))
-
-    item_to_delete = path_list[-1]
-    is_array, key, index = has_valid_brackets(item_to_delete)
-    try:
-        if is_array:
-            item_parent[key][index]  # to trigger a KeyError if not found
-            delete_with_comments_preservation(item_parent[key], index)
-        else:
-            item_parent[item_to_delete]
-            delete_with_comments_preservation(item_parent, item_to_delete)
-    except (AttributeError, KeyError, IndexError, TypeError):
-        print("An error occurred when deleting '{}' :".format(item_to_delete))
-        raise
+    output_data, _ = delete_yaml_item(data, args.path_to_key, True)
 
     output_file = open(args.output, 'w') if args.output else sys.stdout
-    round_trip_dump(data, output_file, indent=args.indent)
+    round_trip_dump(output_data, output_file)
+    output_file.close()
+
+
+def comment_command():  # pragma: no cover
+    """
+    Sub-command, see main()
+    """
+    # TODO: refactor this command with delete ?
+    parser = argparse.ArgumentParser(
+        description='Comment one item from the input yaml file')
+    parser.add_argument('path_to_key', type=str, nargs='+',
+                        help='<Required> Yaml item to be commented, e.g. "foo 0 bar"')
+    parser.add_argument('-i', '--input', type=str,
+                        help='<Required> Path to the input yaml file', required=True)
+    parser.add_argument('-o', '--output', type=str,
+                        help='Path to the output file, or stdout by default')
+    parser.add_argument('--indent', type=int,
+                        help='Number of space(s) for each indent', default=2)
+
+    args = parser.parse_args(sys.argv[2:])
+    input_file = open(args.input, 'r')
+    data = round_trip_load(input_file.read(), preserve_quotes=True)
+    input_file.close()
+
+    output_data = comment_yaml_item(data, args.path_to_key, True)
+
+    output_file = open(args.output, 'w') if args.output else sys.stdout
+    round_trip_dump(output_data, output_file)
     output_file.close()
 
 
